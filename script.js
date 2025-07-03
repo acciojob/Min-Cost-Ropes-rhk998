016b063b8ccdba58236115683044fc4dbacd3b58
rhk998 <130037816+rhk998@users.noreply.github.com>
2025-07-03 13:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	const heap = [];
+	let totalCost = 0;
+
+	for(let length of arr){
+		heap.push(length);
+	}
+
+	heap.sort((a,b) => a - b);
+
+	while(heap.length > 1){
+		const r1 = heap.shift();
+		const r2 = heap.shift();
+		const cost = r1 + r2;
+		heap.push(cost);
+		totalCost+=r1 + r2;
+		heap.sort((a, b) => a - b);
+	}
+	return totalCost;
   
 }
 
